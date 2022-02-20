7cc46d3b137753eba5975180bc0794ce9acc7b15
krishna98967 <krishnavidya212@gmail.com>
2022-02-20 22:41:18 +0530

    ading the vpc id and name

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "VPC" {
-  cidr_block           = var.cidr_block
+  cidr_block           = "10.60.0.0/16"
   enable_dns_support   = "true" #gives you an internal domain name
   enable_dns_hostnames = "true" #gives you an internal host name
   enable_classiclink   = "false"
   tags = {
-    Name      = var.vpcname
+    Name      = "spiderman"
     owner     = "krishnareddy"
     operation = "production"
   }
