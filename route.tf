f349f104e573b6748c800dd28ae7f6b5f9e82de5
krishna98967 <krishnavidya212@gmail.com>
2022-02-22 07:36:57 +0530

    adding routefile

---
+++ route.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "public_route" {
+    vpc_id = "${aws_vpc.VPC.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.IGW.id}"
+    }
+
+    tags = {
+        Name = "Public Route"
+    }
+}
+
+resource "aws_route_table_association" "routing" {
+    subnet_id = "${aws_subnet.public_subnet1.id}"
+    route_table_id = "${aws_route_table.public_route.id}"
+}
