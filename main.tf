0df52a9baa080ad810ba1aec9fe0956c5cde017d
Krishnareddy Bhumireddy <96362018+krishna98967@users.noreply.github.com>
2022-03-23 12:49:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 #this is my new terraform code for vpc creation 
 provider "aws" {
   region = "us-east-1"
+  owner="krishnareddy"
   #access_key = var.access_key
   #cmd
   #secret_key = var.secret_key
